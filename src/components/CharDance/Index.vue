1b2e71cd0410e7b89e6ac2e8e98cd2ace0b6d0cf
wszxdhr <wszxdhr@126.com>
2020-03-09 23:22:04 +0800

    feat(首页组件):添加字符画组件

---
+++ src/components/CharDance/Index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="char-dance">
+    <div class="char-dance-row" v-for="(row, rowIndex) in rectDisplay" :key="rowIndex">
+      <pre class="char-dance-item" v-for="(char, charIndex) in row" :key="charIndex">{{char}}</pre>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CharDance',
+  props: {
+    rect: {
+      default: () => ([]),
+      type: Array
+    },
+    animateOnInit: {
+      default: false,
+      type: Boolean
+    },
+    transChar: {
+      default: '+',
+      type: [String, Array]
+    }
+  },
+  methods: {
+    getRandomInt () {},
+    play () {},
+    stop () {}
+  },
+  computed: {
+    rectDisplay () {
+      return this.rect
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .char-dance {
+    line-height: 1;
+    &-row {
+      text-align: left;
+      display: flex;
+    }
+    &-item {
+      margin: 0;
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <img alt="Vue logo" src="../assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <char-dance :rect="[[' ', ' ', '3', ' ', ' ', ' ', ' ', ' ', ' '], [' ', '6', '7', '8', 'a', 'b', 'c', 'd', 'e'], [' ', '0', '1', '2', 'a', 'b', 'c', 'd', 'e'], [' ', ' ', '5', ' ', ' ', ' ', ' ', ' ', ' ']]"
+                :trans-char="['!', '@', '#', '$', '%']"
+                animate-on-init></char-dance>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CharDance from '@/components/CharDance/Index'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    CharDance
   }
 }
 </script>
+
+<style lang="scss">
+  .home {
+    display: flex;
+    justify-content: center;
+  }
+</style>
