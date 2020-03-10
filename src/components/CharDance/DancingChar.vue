999de0fafac35ec9f88eb11def7703d3d53e11fe
wszxdhr <wszxdhr@126.com>
2020-03-10 08:55:17 +0800

    feat(CharDance):初步完成charDance动画

---
+++ src/components/CharDance/DancingChar.vue
@@ -0,0 +1,32 @@
+<template>
+  <span>
+    {{ char }}
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'DancingChar',
+  props: {
+    chars: {
+      type: [Array, String],
+      default: '@#$%^&*()'
+    }
+  },
+  data () {
+    return {
+      char: this.chars[0],
+      charIndex: 0
+    }
+  },
+  created () {
+    setInterval(() => {
+      this.charIndex++
+      if (this.charIndex > this.chars.length) {
+        this.charIndex = 0
+      }
+      this.char = this.chars[this.charIndex]
+    }, 50)
+  }
+}
+</script>
--- src/components/CharDance/Index.vue
+++ src/components/CharDance/Index.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="char-dance">
     <div class="char-dance-row" v-for="(row, rowIndex) in rectDisplay" :key="rowIndex">
-      <pre class="char-dance-item" v-for="(char, charIndex) in row" :key="charIndex">{{char}}</pre>
+      <pre class="char-dance-item" v-for="(char, charIndex) in row" :key="charIndex">
+        <span v-if="!danceList.includes(`${rowIndex},${charIndex}`)">{{char}}</span>
+        <dancing-char v-if="danceList.includes(`${rowIndex},${charIndex}`)" :chars="transChar"></dancing-char>
+      </pre>
     </div>
   </div>
 </template>
 
 <script>
+import DancingChar from './DancingChar'
 export default {
   name: 'CharDance',
+  components: {
+    DancingChar
+  },
   props: {
     rect: {
       default: () => ([]),
@@ -23,14 +30,93 @@ export default {
       type: [String, Array]
     }
   },
+  data () {
+    return {
+      timers: [],
+      isDancing: false,
+      useOldChars: true,
+      oldChars: this.rect,
+      danceList: []
+    }
+  },
+  created () {
+    if (this.animateOnInit) {
+      this.play()
+    }
+  },
   methods: {
-    getRandomInt () {},
-    play () {},
-    stop () {}
+    getRandomInt (start, end) {
+      return Math.round(start + (end - start) * Math.random())
+    },
+    sleep (time) {
+      return new Promise(resolve => {
+        this.timers.push(setTimeout(() => {
+          resolve()
+        }, time))
+      })
+    },
+    // 乱序列表
+    randomList (list) {
+      const temp = [...list]
+      const result = []
+      while (temp.length) {
+        result.push(...(temp.splice(this.getRandomInt(0, temp.length - 1), 1) || []))
+      }
+      return result
+    },
+    async play () {
+      if (!this.isDancing) {
+        this.isDancing = true
+        this.useOldChars = true
+        // 获取二维数组的角标，每次setTimeout时随机选一个
+        let time = 0
+        // 如果用for of 循环，因为改变了源数组，所以会循环不完，所以复制一个数组出来用
+        console.log(this.randomList(this.flatRectIndex))
+        for (const indexArr of this.randomList(this.flatRectIndex)) {
+          this.timers.push(setTimeout(() => {
+            this.danceList.push(indexArr.join(','))
+          }, time))
+          time += this.getRandomInt(10, 50)
+        }
+        this.timers.push(setTimeout(() => {
+          this.stop()
+        }, time))
+      }
+    },
+    async stop () {
+      if (this.isDancing) {
+        for (const timer of this.timers) {
+          clearTimeout(timer)
+        }
+        this.useOldChars = false
+        this.isDancing = false
+        const copiedDanceList = this.randomList(this.danceList)
+        // console.log(copiedDanceList.length)
+        while (this.danceList.length) {
+          const deleteItem = copiedDanceList.pop()
+          await this.sleep(this.getRandomInt(10, 50))
+          this.danceList.splice(this.danceList.indexOf(deleteItem), 1)
+        }
+      }
+    }
   },
   computed: {
+    flatRectIndex () {
+      return this.rect.map((rectItem, rectIndex) => {
+        if (rectItem instanceof Array) {
+          return rectItem.map((subItem, subIndex) => [rectIndex, subIndex])
+        } else {
+          return []
+        }
+      }).flat()
+    },
     rectDisplay () {
       return this.rect
+    }
+  },
+  watch: {
+    rect (val, oldVal) {
+      this.oldChars = oldVal
     }
   }
 }
@@ -45,6 +131,8 @@ export default {
     }
     &-item {
       margin: 0;
+      white-space: normal;
+      width: 20px;
     }
   }
 </style>
