4fbbe9fa84d874df4449c966a02a4526c4c8bfd3
wszxdhr <wszxdhr@126.com>
2020-03-26 23:45:35 +0800

    style:更换邮箱名，feat(IntroDialog):修改个人信息样式

---
+++ src/components/Dialog/pointInPolygon.js
@@ -0,0 +1,5 @@
+// 暂时只做凸四边形的
+
+export default function ({ x = null, y = null } = {}, polygon = []) {
+
+}
--- src/components/SubDialogs/Intro/index.vue
+++ src/components/SubDialogs/Intro/index.vue
@@ -2,8 +2,9 @@
   <div class="intro-dialog-wrap">
     <hp-dialog :visible="visible"
                @update:visible="$emit('update:visible', $event)"
+               ref="mainDialog"
                class="intro-dialog"
-               left="460px" top="calc((100vh - 500px) / 2)" width="360px"
+               :left="`calc(50vw - (${mainDialogWidth}px + ${detailDialogWidth}px) / 2)`" top="calc((50vh - 160px) / 2)" :width="`${mainDialogWidth}px`"
                title="爱我中华">
       <hp-sub-dialog title="PERSONAL INFORMATION">
         <div class="intro-dialog-inner">
@@ -12,26 +13,44 @@
           </p>
           <img src="@/assets/images/Person.jpg" alt="" class="person-image"/>
           <p class="gender-and-age">
-            <span class="text-color-gray-light text-size-common">男</span>
-            <span class="text-color-gray-light text-size-small age">26岁</span>
+            <span class="text-color-gray-very-light text-size-common">男</span>
+            <span class="text-color-gray-very-light text-size-small age">26岁</span>
           </p>
-          <p class="text-color-gray-light text-size-small">本科统招（网络工程）</p>
-          <p class="text-color-gray-light text-size-small">4年工作经验</p>
-          <p class="text-color-gray-light text-size-small">EMAIL ADDRESS</p>
+          <p class="text-color-gray-very-light text-size-small">本科统招（网络工程）</p>
+          <p class="text-color-gray-very-light text-size-small">4年工作经验</p>
+          <p class="text-color-gray-very-light text-size-small">EMAIL ADDRESS</p>
           <img src="@/assets/images/NameBarcode.png" alt="" class="name-barcode-image"/>
-          <p class="text-color-gray-light text-size-small tac">
-            <a href="mailto:jobs@anymelon.com">jobs@anymelon.com</a>
+          <p class="text-color-gray-very-light text-size-small tac">
+            <a href="mailto:jobs@anymelon.com">job@anymelon.com</a>
           </p>
         </div>
       </hp-sub-dialog>
     </hp-dialog>
     <hp-dialog :visible="visible"
                @update:visible="$emit('update:visible', $event)"
+               ref="detailDialog"
                class="intro-dialog-detail"
-               left="840px" top="calc((100vh - 500px) / 2)" width="500px"
+               :left="`calc(50vw - (${mainDialogWidth}px + ${detailDialogWidth}px) / 2 + ${mainDialogWidth}px + 20px)`" top="calc((50vh - 160px) / 2)" :width="`${detailDialogWidth}px`"
                title="爱我中华">
       <hp-sub-dialog title="PERSONAL INFORMATION">
-        123
+        <h2 class="text-size-common text-color-primary intro-detail-title">
+          技能
+        </h2>
+        <p class="text-size-small text-color-gray-very-light intro-detail-content">
+          划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水，划水划水划水划水划水划水划水划水划水划水划水划水划水。划水划水划水划水划水划水划水划水划水。
+        </p>
+        <h2 class="text-size-common text-color-primary intro-detail-title">
+          技能
+        </h2>
+        <p class="text-size-small text-color-gray-very-light intro-detail-content">
+          划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水，划水划水划水划水，划水划水划水划水，划水划水划水划水，划水划水划水划水划水划水划水划水划水划水划水划水划水。划水划水划水划水划水划水划水划水划水。
+        </p>
+        <h2 class="text-size-common text-color-primary intro-detail-title">
+          技能
+        </h2>
+        <p class="text-size-small text-color-gray-very-light intro-detail-content">
+          划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水，划水划水水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水，划水划水水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水划水，划水划水划水划水划水划水划水划水划水划水划水划水划水。划水划水划水划水划水划水划水划水划水。
+        </p>
       </hp-sub-dialog>
     </hp-dialog>
   </div>
@@ -44,6 +63,20 @@ export default {
     visible: {
       type: Boolean,
       default: false
+    }
+  },
+  data () {
+    return {
+      mainDialogWidth: 360,
+      detailDialogWidth: 600
+    }
+  },
+  watch: {
+    visible (val) {
+      if (val && this.$refs && this.$refs.mainDialog && this.$refs.detailDialog) {
+        this.$refs.detailDialog.setActive(true)
+        this.$refs.mainDialog.setActive(true)
+      }
     }
   }
 }
@@ -85,6 +118,18 @@ export default {
         & + p {
           margin-top: 14px;
         }
+      }
+    }
+  }
+  .intro-dialog-detail {
+    .intro-detail-title {
+      & + .intro-detail-content {
+        margin-top: 4px;
+      }
+    }
+    .intro-detail-content {
+      & + .intro-detail-title {
+        margin-top: 20px;
       }
     }
   }
