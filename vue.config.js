5353dbc4ce089d31822cc630297f444d64b18f08
wszxdhr <wszxdhr@126.com>
2020-03-26 18:34:31 +0800

    chore:publicPath根据env切换

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   devServer: {
     port: 8822
   },
-  publicPath: 'https://wszxdhr.github.io/homepage'
+  publicPath: process.env.NODE_ENV === 'production' ? 'https://wszxdhr.github.io/homepage' : ''
 }
