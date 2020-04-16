40185882cc1013cb3527e6fa357c8ba33fdcef61
wszxdhr <wszxdhr@126.com>
2020-04-16 22:20:12 +0800

    chore(生成PDF):修改生成的脚本

--- genPdf.js
+++ genPdf.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const { fork } = require('child_process')
 const puppeteer = require('puppeteer')
+const process = require('process')
 
 fork(path.join(__dirname, './staticServer.js'));
 
@@ -8,7 +9,9 @@ fork(path.join(__dirname, './staticServer.js'));
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto('http://localhost:9292/#/pdf')
-  await page.pdf({ path: path.join(__dirname, './dist/张潇的简历.pdf'), format: 'A4' })
+  await page.pdf({ path: path.join(__dirname, './public/张潇的简历.pdf'), format: 'A4' })
 
   await browser.close()
+
+  process.exit()
 })()
