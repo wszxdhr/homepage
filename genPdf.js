a0f3091d3c894187a200295afc4bb294cee30011
wszxdhr <wszxdhr@126.com>
2020-04-16 22:31:59 +0800

    chore(puppeteer):更换puppeteer action工具

--- genPdf.js
+++ genPdf.js
@@ -6,7 +6,9 @@ const process = require('process')
 fork(path.join(__dirname, './staticServer.js'));
 
 (async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+    headless: true
+  })
   const page = await browser.newPage()
   await page.goto('http://localhost:9292/#/pdf')
   await page.pdf({ path: path.join(__dirname, './public/张潇的简历.pdf'), format: 'A4' })
