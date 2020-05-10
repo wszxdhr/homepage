14aa8beee953b78f01cc81cee3e085087a7d22aa
wszxdhr <wszxdhr@126.com>
2020-05-10 21:33:53 +0800

    chore(七牛):修改上传脚本

--- qiniuUpload.js
+++ qiniuUpload.js
@@ -84,39 +84,36 @@ const uploadFile = (filePath, fileName) => {
   })
 }
 
+const uploadList = []
+
 /**
  * 文件遍历方法
  * @param filePath 需要遍历的文件路径
  */
 function fileDisplay (filePath) {
   // 根据文件路径读取文件，返回文件列表
-  fs.readdir(filePath, function (err, files) {
-    if (err) {
-      console.warn(err)
-    } else {
-      // 遍历读取到的文件列表
-      files.forEach(function (filename) {
-        // 获取当前文件的绝对路径
-        const filedir = path.join(filePath, filename)
-        // 根据文件路径获取文件信息，返回一个fs.Stats对象
-        fs.stat(filedir, function (eror, stats) {
-          if (eror) {
-            console.warn('获取文件stats失败')
-          } else {
-            const isFile = stats.isFile() // 是文件
-            const isDir = stats.isDirectory() // 是文件夹
-            if (isFile) {
-              console.log('正在尝试上传：' + filename)
-              uploadFile(filedir, filedir.replace(distDir + '/', ''))
-            }
-            if (isDir) {
-              fileDisplay(filedir) // 递归，如果是文件夹，就继续遍历该文件夹下面的文件
-            }
-          }
-        })
-      })
+  const files = fs.readdirSync(filePath)
+  // 遍历读取到的文件列表
+  files.forEach(function (filename) {
+    // 获取当前文件的绝对路径
+    const filedir = path.join(filePath, filename)
+    // 根据文件路径获取文件信息，返回一个fs.Stats对象
+    const stats = fs.statSync(filedir)
+    const isFile = stats.isFile() // 是文件
+    const isDir = stats.isDirectory() // 是文件夹
+    if (isFile) {
+      console.log('正在尝试上传：' + filename)
+      uploadList.push([filedir, filedir.replace(distDir + '/', '')])
+    }
+    if (isDir) {
+      fileDisplay(filedir) // 递归，如果是文件夹，就继续遍历该文件夹下面的文件
     }
   })
 }
 
 fileDisplay(distDir)
+for (let index = 0; index < uploadList.length; index++) {
+  setTimeout(() => {
+    uploadFile(...uploadList.pop())
+  }, index * 1000)
+}
